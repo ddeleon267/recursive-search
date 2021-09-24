5544720b0c06fe15fef102b08625d4d1bffb334e
ddeleon267 <dominique.a.deleon@gmail.com>
2021-09-24 06:09:44 -0400

    Adds js sol'n.

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,6 +1,25 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  if (arr.length === 0) return false
+  const first = arr.shift()
+  if (first === target) return true
+  return recursiveSearch(arr, target)
+
 }
+
+// pseudocode
+// return if there's nothing left in array (return what?)
+
+// grab first value and remove from array
+// if it's equal to target, return true
+// else, make recursive call and pass in target and modified array
+
+
+// Rewrite the problem in your own words
+// given an array and a target integer, return "true"
+// if the array includes the integer
+// else, return false
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -11,6 +30,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", recursiveSearch([3, 2, 1], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", recursiveSearch([1, 5, 6], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", recursiveSearch([3, 2, 2], 2));
 }
 
 module.exports = recursiveSearch;
